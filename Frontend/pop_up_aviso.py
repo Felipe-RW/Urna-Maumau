18ab96cae9ae74194888bc3d307cde6f842c041c
joao77734 <oshymist@gmail.com>
2026-09-25 16:08:14 -0400

    First Commit

---
+++ Frontend/pop_up_aviso.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMessageBox
+
+app = QApplication(sys.argv)
+
+msg = QMessageBox()
+msg.setIcon(QMessageBox.Warning)
+msg.setWindowTitle("Confirmar voto")
+msg.setText("Deseja confirmar voto?")
+msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+msg.button(QMessageBox.Yes).setText("Confirmar")
+msg.button(QMessageBox.No).setText("Cancelar")
+
+resposta = msg.exec()
+
+sys.exit(app.exec())
+#PopUp de aviso de confirmar voto
